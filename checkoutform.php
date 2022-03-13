43f9c9f271615b81cb92e5b2d177677bd5b745fc
Hyebin Yoon <hyebin.yoon@ryerson.ca>
2022-03-13 19:49:28 -0400

    sdf

--- checkoutform.php
+++ checkoutform.php
@@ -3,15 +3,18 @@
 <html>
     <head>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
-    <script
-      src="https://maps.googleapis.com/maps/api/js?key=AIzaSyB_Mz30zz7nncfQho_Nrqc9kHrlNQKhfG0&callback=initMap&v=weekly"
-      async
+    <script 
+      src="https://maps.googleapis.com/maps/api/js?key=AIzaSyB_Mz30zz7nncfQho_Nrqc9kHrlNQKhfG0&callback=initMap&libraries=&v=weekly"
+     async
     ></script>
         <!-- <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyB_Mz30zz7nncfQho_Nrqc9kHrlNQKhfG0"></script> -->
         <script type="text/javascript">
      
 let longAdd = "";
 let latAdd = "";
+var map;
+var isMapEmpty = true;
+var directionsDisplay;
 function codeAddress() {
     geocoder = new google.maps.Geocoder();
     var address = document.getElementById("my-address").value;
@@ -21,8 +24,56 @@ function codeAddress() {
       longAdd = results[0].geometry.location.lat();
      
       latAdd = results[0].geometry.location.lng();
-      alert("Long is : "+longAdd+" Lat is : "+ latAdd);
-      } 
+  
+
+      
+      let branch = document.getElementById("branch");
+    let origin = new google.maps.LatLng(longAdd,latAdd);
+    let destination = '' ;
+    switch (branch.value){
+      case "zach":
+        alert("Your order has been shipped to Branch1: Zach Corp, \n please use the map the pick your item up");
+        destination=  new google.maps.LatLng(43.740540,-79.321830);
+        break;
+      case "addy":
+        alert("Your order has been shipped to Branch2: Company Addy, \n please use the map the pick your item up");
+        destination=  new google.maps.LatLng(43.096249, -79.076973);
+        break;
+      case "hyebin":
+        alert("Your order has been shipped to Branch3: Hey Bin Org, \n please use the map the pick your item up");
+        destination=  new google.maps.LatLng( 43.836921,-79.540896);
+        break;
+
+    }
+    var directionsService = new google.maps.DirectionsService();
+  var directionsRenderer = new google.maps.DirectionsRenderer();
+ directionsDisplay = new google.maps.DirectionsRenderer();
+
+
+  // it should clear the routes ... but its not working
+if (directionsDisplay != null && !isMapEmpty) {
+  console.log('Sibal Fuck you');
+    directionsDisplay.setMap(null);
+    directionsDisplay = null;
+    isMapEmpty = true;
+    
+}
+    console.log(origin+" pog "+ destination);
+    isMapEmpty = false;
+  var request = { 
+            origin: origin,
+            destination: destination,
+            travelMode: 'DRIVING'
+};
+  directionsService.route(request, function(result, status) {
+    if (status == 'OK') {
+      console.log("yeet")
+      directionsRenderer.setDirections(result);
+    }
+  });
+  directionsRenderer.setMap(map);
+
+      }
       else {
         alert("Geocode was not successful for the following reason: " + status);
       }
@@ -33,7 +84,7 @@ function codeAddress() {
 <script src="./checkoutform.js" defer></script>
   </head>
 <body>
-<div id="mapTest">Supfsdfsdfds</div>
+
 <div class = "constainer">
     <form>
   <div class="form-row">
@@ -80,10 +131,10 @@ function codeAddress() {
       <input type="text" class="form-control" id="inputZip">
     </div>
   </div>
-  <select class="custom-select" multiple>
-  <option selected>Branch1 : Zach Coporation</option>
-  <option value="1">Branch2 : Company Addy </option>
-  <option value="2">Branch3 : Hey Bin Organizationn</option>
+  <select class="custom-select" id = "branch" multiple>
+  <option value="zach">Branch1 : Zach Coporation</option>
+  <option value="addy">Branch2 : Company Addy </option>
+  <option value="hyebin">Branch3 : Hey Bin Organizationn</option>
 
 </select>
 </form>
@@ -94,18 +145,25 @@ function codeAddress() {
 <script>
 //Showing map :D
       function initMap(){
-        var options = {
-          zoom:8,
-          center:{lat:-79.4616396, lng:43.7613314}
-        }
-        var map = new google.maps.Map(document.getElementById("mapTest"), options);
-        
-        // var marker = new google.maps.Marker({
-        //   position: {lat:43.6426, lng:79.3871 }
-        //   map: map
-        // })
-      }
-      </script>
-        
+       
+        const myLatLng = {lat:43.657650, lng:-79.377388};
 
-</body>
+        map = new google.maps.Map(document.getElementById("mapTest"), {
+          zoom: 15,
+          center: myLatLng,
+        });
+        new google.maps.Marker({
+          position : myLatLng,
+          map,
+          title: "Sup",
+
+        });
+       
+
+      }
+
+ 
+</script>
+<div id="mapTest" style="position:relative; overflow:hidden; height: 500px; width: 500px; "></div>
+  </body>
+</html>
--- home.php
+++ home.php
@@ -8,10 +8,183 @@
       integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm"
       crossorigin="anonymous"
     />
-    <link rel="stylesheet" href="styles.css" />
+    <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+    <link rel="stylesheet" href="bootstyles.css" />
+    <script src="https://use.fontawesome.com/releases/v5.15.4/js/all.js" crossorigin="anonymous"></script>
+     <!-- Google fonts-->
+     <link href="https://fonts.googleapis.com/css?family=Varela+Round" rel="stylesheet" />
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet" />
+        <!-- Core theme CSS (includes Bootstrap)-->
+        
     <title>Iter 1</title>
   </head>
-  <body>
-    <h1>Assumed kitchen items for our site</h1>
+  <body id="page-top">
+     <!-- Masthead-->
+     <header class="masthead">
+            <div class="container px-4 px-lg-5 d-flex h-100 align-items-center justify-content-center">
+                <div class="d-flex justify-content-center">
+                    <div class="text-center">
+                        <h1 class="mx-auto my-0 text-uppercase">Group 26 Appliance Page</h1>
+                        <h2 class="text-white-50 mx-auto mt-2 mb-5">Ryerson University Department of Computer Science CPS630 Web Applications Winter2022 </h2>
+                        <a class="btn btn-primary" href="index.php?services">Get Started</a>
+                    </div>
+                </div>
+            </div>
+        </header>
+        <!-- About-->
+        <section class="about-section text-center" id="about">
+            <div class="container px-4 px-lg-5">
+                <div class="row gx-4 gx-lg-5 justify-content-center">
+                    <div class="col-lg-8">
+                        <h2 class="text-white mb-4">Best Appliances in Toronto</h2>
+                        <p class="text-white-50">
+                            Dr. Meow highly recommend to buy your next dream house appliance from us!
+                            <a href="https://www.lovemeow.com/">Dr.Meow's page</a>
+                            Meow Meow Meow Meow Meow Meow Meow Meow Meow Meow Meow Meow Meow Meow Meow Meow 
+                        </p>
+                    </div>
+                </div>
+                <img class="img-fluid" src="assets/img/ipad.png" alt="..." />
+            </div>
+        </section>
+        <!-- Projects-->
+        <section class="projects-section bg-light" id="projects">
+            <div class="container px-4 px-lg-5">
+                <!-- Featured Project Row-->
+                <div class="row gx-0 mb-4 mb-lg-5 align-items-center">
+                    <div class="col-xl-8 col-lg-7"><img class="img-fluid mb-3 mb-lg-0" src="assets/img/bg-masthead.jpg" alt="..." /></div>
+                    <div class="col-xl-4 col-lg-5">
+                        <div class="featured-text text-center text-lg-left">
+                            <h4>Group 26</h4>
+                            <p class="text-black-50 mb-0">Group 26 members includes Zachary Risling 500901836 / Adnan Rehman 500840171 / Hyebin Yoon 50084665. Everyone made equal contribution to the project to produce the best result.  </p>
+                        </div>
+                    </div>
+                </div>
+                <!-- Project One Row-->
+                <div class="row gx-0 mb-5 mb-lg-0 justify-content-center">
+                    <div class="col-lg-6"><img class="img-fluid" src="assets/img/demo-image-01.jpg" alt="..." /></div>
+                    <div class="col-lg-6">
+                        <div class="bg-black text-center h-100 project">
+                            <div class="d-flex h-100">
+                                <div class="project-text w-100 my-auto text-center text-lg-left">
+                                    <h4 class="text-white">Screaming Cat</h4>
+                                    <p class="mb-0 text-white-50"><a href="https://knowyourmeme.com/memes/screaming-cat">Screaming Cat, </a>also known as Yamato Cat, refers to a reaction image series of a photoshopped image of a cat crying. </p>
+                                    <hr class="d-none d-lg-block mb-0 ms-0" />
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!-- Project Two Row-->
+                <div class="row gx-0 justify-content-center">
+                    <div class="col-lg-6"><img class="img-fluid" src="assets/img/demo-image-02.jpg" alt="..." /></div>
+                    <div class="col-lg-6 order-lg-first">
+                        <div class="bg-black text-center h-100 project">
+                            <div class="d-flex h-100">
+                                <div class="project-text w-100 my-auto text-center text-lg-right">
+                                    <h4 class="text-white">Woman Yelling at a Cat </h4>
+                                    <p class="mb-0 text-white-50"><a href="https://knowyourmeme.com/memes/woman-yelling-at-a-cat">Woman Yelling at a Cat, </a>Woman Yelling at a Cat refers to a meme format featuring a screen cap of The Real Housewives of Beverly Hills cast members Taylor Armstrong and Kyle Richards followed by a picture of a confused-looking cat sitting behind a dinner plate.</p>
+                                    <hr class="d-none d-lg-block mb-0 me-0" />
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!-- Signup-->
+        <section class="signup-section" id="signup">
+            <div class="container px-4 px-lg-5">
+                <div class="row gx-4 gx-lg-5">
+                    <div class="col-md-10 col-lg-8 mx-auto text-center">
+                        <i class="far fa-paper-plane fa-2x mb-2 text-white"></i>
+                        <h2 class="text-white mb-5">Subscribe to receive updates!</h2>
+                        <!-- * * * * * * * * * * * * * * *-->
+                        <!-- * * SB Forms Contact Form * *-->
+                        <!-- * * * * * * * * * * * * * * *-->
+                        <!-- This form is pre-integrated with SB Forms.-->
+                        <!-- To make this form functional, sign up at-->
+                        <!-- https://startbootstrap.com/solution/contact-forms-->
+                        <!-- to get an API token!-->
+                        <form class="form-signup" id="contactForm" data-sb-form-api-token="API_TOKEN">
+                            <!-- Email address input-->
+                            <div class="row input-group-newsletter">
+                                <div class="col"><input class="form-control" id="emailAddress" type="email" placeholder="Enter email address..." aria-label="Enter email address..." data-sb-validations="required,email" /></div>
+                                <div class="col-auto"><button class="btn btn-primary disabled" id="submitButton" type="submit">Notify Me!</button></div>
+                            </div>
+                            <div class="invalid-feedback mt-2" data-sb-feedback="emailAddress:required">An email is required.</div>
+                            <div class="invalid-feedback mt-2" data-sb-feedback="emailAddress:email">Email is not valid.</div>
+                            <!-- Submit success message-->
+                            <!---->
+                            <!-- This is what your users will see when the form-->
+                            <!-- has successfully submitted-->
+                            <div class="d-none" id="submitSuccessMessage">
+                                <div class="text-center mb-3 mt-2 text-white">
+                                    <div class="fw-bolder">Form submission successful!</div>
+                                    Out of Service
+                                    <br />
+                                   
+                                </div>
+                            </div>
+                            <!-- Submit error message-->
+                            <!---->
+                            <!-- This is what your users will see when there is-->
+                            <!-- an error submitting the form-->
+                            <div class="d-none" id="submitErrorMessage"><div class="text-center text-danger mb-3 mt-2">Error sending message!</div></div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!-- Contact-->
+        <section class="contact-section bg-black">
+            <div class="container px-4 px-lg-5">
+                <div class="row gx-4 gx-lg-5">
+                    <div class="col-md-4 mb-3 mb-md-0">
+                        <div class="card py-4 h-100">
+                            <div class="card-body text-center">
+                                <i class="fas fa-map-marked-alt text-primary mb-2"></i>
+                                <h4 class="text-uppercase m-0">Address</h4>
+                                <hr class="my-4 mx-auto" />
+                                <div class="small text-black-50">123 Main Street, Toronto ON </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-4 mb-3 mb-md-0">
+                        <div class="card py-4 h-100">
+                            <div class="card-body text-center">
+                                <i class="fas fa-envelope text-primary mb-2"></i>
+                                <h4 class="text-uppercase m-0">Email</h4>
+                                <hr class="my-4 mx-auto" />
+                                <div class="small text-black-50"><a href="#!">fakeEmail@fakedomain.com</a></div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-4 mb-3 mb-md-0">
+                        <div class="card py-4 h-100">
+                            <div class="card-body text-center">
+                                <i class="fas fa-mobile-alt text-primary mb-2"></i>
+                                <h4 class="text-uppercase m-0">Phone</h4>
+                                <hr class="my-4 mx-auto" />
+                                <div class="small text-black-50">+1 (234) 567-8910</div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="social d-flex justify-content-center">
+                    <a class="mx-2" href="#"><i class="fab fa-twitter"></i></a>
+                    <a class="mx-2" href="#"><i class="fab fa-facebook-f"></i></a>
+                    <a class="mx-2" href="#"><i class="fab fa-github"></i></a>
+                </div>
+            </div>
+        </section>
+        <!-- Footer-->
+        <footer class="footer bg-black small text-center text-white-50"><div class="container px-4 px-lg-5">Social Media Icon &copy; Out of Service</div></footer>
+        <!-- Bootstrap core JS-->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+        <!-- Core theme JS-->
+        <script src="scripts.js"></script>
+
+        <script src="https://cdn.startbootstrap.com/sb-forms-latest.js"></script>
   </body>
 
